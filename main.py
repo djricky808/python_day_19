643d9d6a99aeda7d542e77c44a6558165ac583b1
Richard Olpindo <djricky088@yahoo.com>
2026-04-29 14:11:51 -1000

    Created event listeners for navigation

--- main.py
+++ main.py
@@ -6,7 +6,25 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
+
+def turn_clockwise():
+    tim.right(10)
+
+def turn_counterclockwise():
+    tim.left(10)
+
+def clear_drawing():
+    screen.reset()
+    tim.teleport(0)
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards,'w')
+screen.onkey(move_backwards, 's')
+screen.onkey(turn_clockwise,'a')
+screen.onkey(turn_counterclockwise, 'd')
+screen.onkey(clear_drawing,'c')
 
 screen.exitonclick()
